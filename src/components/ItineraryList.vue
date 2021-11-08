b64b5187b0461bca8fa8744969415f05fb348c19
Gavin Steck <gavinmsteck@gmail.com>
2021-11-07 22:24:51 -0700

    updated json

--- src/components/ItineraryList.vue
+++ src/components/ItineraryList.vue
@@ -11,7 +11,9 @@
           <p>{{ park.Size }} acres</p>
         </div>
         <div class="add">
-          <button class="auto" @click="remove(park)">Remove from Itinerary</button>
+          <button class="auto" @click="remove(park)">
+            Remove from Itinerary
+          </button>
         </div>
       </div>
     </div>
@@ -27,6 +29,31 @@ export default {
   methods: {
     remove(park) {
       this.$root.$data.parkList.splice(park, 1);
+    },
+    distance(lat1, lat2, lon1, lon2) {
+      // The math module contains a function
+      // named toRadians which converts from
+      // degrees to radians.
+      lon1 = (lon1 * Math.PI) / 180;
+      lon2 = (lon2 * Math.PI) / 180;
+      lat1 = (lat1 * Math.PI) / 180;
+      lat2 = (lat2 * Math.PI) / 180;
+
+      // Haversine formula
+      let dlon = lon2 - lon1;
+      let dlat = lat2 - lat1;
+      let a =
+        Math.pow(Math.sin(dlat / 2), 2) +
+        Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+      let c = 2 * Math.asin(Math.sqrt(a));
+
+      // Radius of earth in kilometers. Use 3956
+      // for miles
+      let r = 6371;
+
+      // calculate the result
+      return c * r;
     },
   },
 };
@@ -76,7 +103,7 @@ button:hover {
   background: rgb(48, 48, 48);
   color: white;
   border: none;
-  border-radius:5%;
+  border-radius: 5%;
 }
 
 button:active {
